1010000716655d58a6daa8f5d6077a8befb5c5ae
Hunter Tsai <as5823934@gmail.com>
2018-08-02 16:48:47 -0700

    add basic timmer

--- Pomodoro/App.js
+++ Pomodoro/App.js
@@ -7,7 +7,10 @@ export default class App extends React.Component {
     this.state={
       mainTime: null,
       lapTime: null,
-      isRunning: false
+      isRunning: false,
+      mainTimeStart: null,
+      lapTimeStart: null,
+      count : 0
     }
   }
 
@@ -22,18 +25,20 @@ export default class App extends React.Component {
     }
     this.setState({
       isRunning: true,
-      mainTimeStart: new Date(),
-      lapTimeStart: new Date()
+      mainTimeStart: new Date().getTime() / 1000,
+      lapTimeStart: new Date(),
     })
+
+    this.interval = setInterval(()=>{
+      this.setState({
+        mainTime: (new Date().getTime() / 1000 - this.state.mainTimeStart + this.state.mainTime).toString().toHHMMSS(),
+        lapTime: new Date() - this.state.lapTimeStart + this.state.lapTime,
+        count: this.state.count + 1
+      });
+    }, 1000);
   }
 
   render() {
-    // setInterval(()=>{
-    //   this.setState({
-    //     mainTime: new Date() - this.state.mainTime
-    //   });
-    // }, 1000);
-
     return (
       <View style={styles.container}>
         <Text>{this.state.mainTime}</Text>
@@ -53,3 +58,15 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+String.prototype.toHHMMSS = function () {
+  var sec_num = parseInt(this, 10); // don't forget the second param
+  var hours   = Math.floor(sec_num / 3600);
+  var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+  var seconds = sec_num - (hours * 3600) - (minutes * 60);
+
+  if (hours   < 10) {hours   = "0"+hours;}
+  if (minutes < 10) {minutes = "0"+minutes;}
+  if (seconds < 10) {seconds = "0"+seconds;}
+  return hours+':'+minutes+':'+seconds;
+}
